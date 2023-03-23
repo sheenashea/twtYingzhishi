faf955df50e62216ccc3f76751f100012e626e9d
sheenashea <3061851662@qq.com>
2023-03-23 19:57:32 +0800

    backup

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import "@/assets/icon/iconfont.css";
 import 'element-ui/lib/theme-chalk/index.css';
-// import ElementUI from 'element-ui';
+//import ElementUI from 'element-ui';
 
 
 createApp(App).use(store).use(router).mount('#app')
--- src/posts/components/NoticeContent.vue
+++ src/posts/components/NoticeContent.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="notice-box">
-        <p>禁止发布任何违法违禁之物，严禁宣传发布交易国家濒危珍惜保护动物骨及角制品。
+        <p class="notice">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;禁止发布任何违法违禁之物，严禁宣传发布交易国家濒危珍惜保护动物骨及角制品。
             如若违反，管理员有权将所发内容屏蔽、删除，并对发言者作出禁言、帐号封禁等处理，
             情况严重者将提交由相关监督部门处理；禁止发布任何商业贴、软广告、招代理、求加盟
             等社区主题，禁止发布低劣恶俗的主题帖，禁止留手机号、微信号、
             微博、微店、二维码等任何其他平台的联系方式。
         </p>
+        <div class="btut">
         <button class="btu" @click="completeread()">阅读完毕</button>
+       </div>
     </div>
 </template>
 
@@ -27,21 +29,35 @@ export default defineComponent({
     position:absolute;
     right:37%;
     top:25%;
-    background-color: lightyellow;
-    border: 2px solid yellow;
+    background-color: white;
+    border: 2px solid gray;
     border-radius: 3px;
     width:300px;
     display: flex;
     flex-direction: column;
 }
+
+.notice {
+    margin: 20px;
+    margin-top: 50px;
+    margin-bottom: 30px;
+}
+
 .btu{
     border: 0;
     border-radius: 5px;
     font-size:10px;
     font-weight: 500;
     color:#fff;
-    background-color: sandybrown;
+    background-color: red;
     cursor:pointer;
     letter-spacing: 2px;
+    width: 130px;
+    height: 30px;
+}
+
+.btut {
+    text-align: center;
+    margin-bottom: 20px;
 }
 </style>
